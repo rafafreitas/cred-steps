988af6454f0d63f05ede56673487ee21bcffe16a
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-21 19:29:53 -0300

    State | Criação do armazém.

---
+++ src/store/modulesApp/getters.js
@@ -0,0 +1,7 @@
+export const isAuth = (state) => {
+    return state.auth
+}
+
+export const getUserType = (state) => {
+    return state.userType
+}
---
+++ src/store/modulesApp/mutations.js
@@ -0,0 +1,7 @@
+export const hasAuth = (state, flag) => {
+    state.auth = flag
+}
+
+export const hasUserType = (state, type) => {
+    state.userType = type
+}
