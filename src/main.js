9025aaa4b42003031290183f3bb5755027ca606e
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-23 10:47:59 -0300

    Router | Iniciado a implementação das rotas.

--- src/main.js
+++ src/main.js
@@ -3,19 +3,23 @@ import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
-
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify, {
     theme: {
-        primary: '#C1543D',
-        secondary: '#b0bec5',
-        accent: '#FDC683',
-        error: '#b71c1c'
+        primary: '#3868c1',
+        secondary: '#c55dbb',
+        accent: '#41fd4f',
+        error: '#b74343'
     }
 })
 
 new Vue({
-  render: h => h(App)
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
