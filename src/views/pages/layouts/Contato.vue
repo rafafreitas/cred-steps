d5d2d2676ef16b0ef845f528623e661037a2cbe4
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-23 10:50:03 -0300

    Views | Criação das rotas internas do sistema.

---
+++ src/views/pages/layouts/Contato.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Contato"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/pages/routes/index.js
@@ -0,0 +1,13 @@
+import Contato from '../layouts/Contato';
+
+export default [
+    {
+        name: 'Contato',
+        path: '/contato',
+        component: Contato,
+        meta: {
+            auth: false,
+            userType: null
+        }
+    }
+]
