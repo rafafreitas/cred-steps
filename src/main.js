a4f28180bf48c61a5b1272989f7a71f1b1b2d30a
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-23 08:59:18 -0300

    Vuetify | Alteração de chamada de plugin.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import '@babel/polyfill'
+import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
-import './plugins/vuetify'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+    theme: {
+        primary: '#C1543D',
+        secondary: '#b0bec5',
+        accent: '#FDC683',
+        error: '#b71c1c'
+    }
+})
 
 new Vue({
   render: h => h(App)
