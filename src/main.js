8b11396bfed072c931c7e9338187aeec3837104d
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-23 17:46:40 -0300

    Contao | Implementado primeiro step da página de contato.
    Utilizado o plugin Vee-Validate para realizar a validação dos formulários.

--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import '@babel/polyfill'
 import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import VeeValidate from 'vee-validate';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(VeeValidate);
 Vue.use(Vuetify, {
     theme: {
-        primary: '#154456',
-        secondary: '#c55dbb',
+        primary: '#02a099',
+        secondary: '#154456',
         accent: '#41fd4f',
         error: '#b74343'
     }
