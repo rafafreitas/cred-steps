65e2e447c12be219017fda4f482c2f1f38e183df
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-26 18:43:57 -0300

    Dictionary | Remoção dos dados do dicionário.

--- src/helpers/dictionary.js
+++ src/helpers/dictionary.js
@@ -3,15 +3,6 @@ const dictionary = {
         required: (field) => `O campo ${field} é obrigatório.`,
     },
     custom: {
-        ocupacao: {
-            required: () => 'Por favor, informe sua ocupação atual!',
-        },
-        nascimento:{
-            required: () => 'Por favor, informe a data do seu nascimento!',
-        },
-        empresa: {
-            required: () => 'Por fafor, informe a empresa que você trabalha!',
-        },
         tratamento: {
             required: () => 'Informe o tratamento!',
         },
