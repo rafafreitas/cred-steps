f6046fa17dbb22e8032e114bea776f934d328f72
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-26 14:41:28 -0300

    Ajuste | Corrigido validações dos campos.

--- src/components/forms/StepperAdicionais.vue
+++ src/components/forms/StepperAdicionais.vue
@@ -3,9 +3,9 @@
         <v-card class="mb-5" height="auto" >
             <!--Para Func. Públicos Estaduais-->
             <div>
-                <h2>Funcionário Público Estadual</h2>
+                <h2>Dados necessários para Funcionários Públicos Estaduais</h2>
                 <p>
-                    Selecione abaixo a forma de nos informar sua margem de empréstimo livre: <br>
+                    Selecione abaixo como vai nos informar sua margem de empréstimo livre: <br>
                     Para descobrir a margem ou ver contracheque acessar
                     <a href="www.peconsig.gov.br" target="_blank">www.peconsig.gov.br</a>
                     e digitar Matrícula e Senha
@@ -18,55 +18,82 @@
                         data-vv-name="margemRadio"
                         :error-messages="errors.collect('margemRadio')" >
                     <v-radio label="Digite sua Margem Livre" value="1"></v-radio>
-
-                        <div v-if="itens.estadual.margemRadio == parseInt(1)" >
-                            <v-text-field
-                                    v-validate="'required'"
-                                    class="text-field-limite margin-top-none"
-                                    v-model="itens.estadual.margem"
-                                    :rules="[() => validInput(itens.estadual.margem) || error]"
-                                    :error-messages="errors.collect('margem')"
-                                    data-vv-name="margem"
-                                    v-money
-                                    required
-                            ></v-text-field>
-                            <p class="error-credito" v-if="showError">Informe o valor da margem.</p>
-                        </div>
-
                     <v-radio label="Matrícula + Senha do Portal PE Consig " value="2"></v-radio>
-
-                        <div v-if="itens.estadual.margemRadio == parseInt(2)" >
-
-                            <v-text-field
-                                    v-validate="'required'"
-                                    :error-messages="errors.collect('Matrícula')"
-                                    data-vv-name="Matrícula"
-                                    v-model="itens.estadual.matricula"
-                                    :rules="[() => validInput(itens.estadual.matricula) || error]"
-                                    label="Matrícula"
-                                    hint="Informe sua matrícula"
-                                    class="text-field-limite"
-                            ></v-text-field>
-
-                            <v-text-field
-                                    v-validate="'required'"
-                                    :error-messages="errors.collect('Senha')"
-                                    data-vv-name="Senha"
-                                    v-model="itens.estadual.password"
-                                    :append-icon="passshow ? 'visibility_off' : 'visibility'"
-                                    :rules="[() => validInput(itens.estadual.password) || error]"
-                                    :type="passshow ? 'text' : 'password'"
-                                    label="Senha"
-                                    hint="Informe a senha do seu portal"
-                                    class="text-field-limite"
-                                    counter
-                                    @click:append="passshow = !passshow"
-                            ></v-text-field>
-
-                        </div>
-
                     <v-radio label="Foto do Contracheque " value="3"></v-radio>
+
                 </v-radio-group>
+
+                <div v-if="itens.estadual.margemRadio === '1'" >
+                    <v-text-field
+                            v-validate="'required'"
+                            class="text-field-limite margin-top-none"
+                            v-model="itens.estadual.margem"
+                            :rules="[() => validInput(itens.estadual.margem) || error]"
+                            :error-messages="errors.collect('margem')"
+                            data-vv-name="margem"
+                            v-money
+                            required
+                            key="input-margin"
+                    ></v-text-field>
+                    <p class="error-credito" v-if="showError">Informe o valor da margem.</p>
+                </div>
+                <div v-if="itens.estadual.margemRadio === '2'" >
+
+                    <v-text-field
+                            v-validate="'required'"
+                            :error-messages="errors.collect('Matricula')"
+                            data-vv-name="Matricula"
+                            v-model="itens.estadual.matricula"
+                            :rules="[() => validInput(itens.estadual.matricula) || error]"
+                            label="Matrícula"
+                            hint="Informe sua matrícula"
+                            class="text-field-limite"
+                            key="input-matricula"
+                    ></v-text-field>
+
+                    <v-text-field
+                            v-validate="'required'"
+                            :error-messages="errors.collect('Senha')"
+                            data-vv-name="Senha"
+                            v-model="itens.estadual.password"
+                            :append-icon="passshow ? 'visibility_off' : 'visibility'"
+                            :rules="[() => validInput(itens.estadual.password) || error]"
+                            :type="passshow ? 'text' : 'password'"
+                            label="Senha"
+                            hint="Informe a senha do seu portal"
+                            class="text-field-limite"
+                            counter
+                            @click:append="passshow = !passshow"
+                            key="input-senha"
+                    ></v-text-field>
+
+                </div>
+                <div v-if="itens.estadual.margemRadio === '3'" >
+
+                    <img height="150"
+                         :src="itens.estadual.file.imageUrl"
+                         v-if="itens.estadual.file.imageUrl"/>
+                    <v-text-field
+                            v-validate="'required'"
+                            :error-messages="errors.collect('Foto-Contracheque')"
+                            data-vv-name="Foto-Contracheque"
+                            v-model="itens.estadual.file.imageName"
+                            :rules="[() => validInput(itens.estadual.file.imageName) || error]"
+                            label="Selecione a imagem."
+                            class="text-field-limite"
+                            @click='pickFile'
+                            prepend-icon='attach_file'
+                            key="input-contracheque"
+                    ></v-text-field>
+
+                    <input type="file"
+                           style="display: none;"
+                           accept="image/*"
+                           capture="camera"
+                           ref="getImage"
+                           @change="onFilePicked"/>
+
+                </div>
 
             </div>
 
@@ -105,9 +132,12 @@
                     margem: "",
                     matricula: "",
                     password: "",
-                    file: "",
+                    file: {
+                        imageName: "",
+                        imageUrl: "",
+                        imageFile: "",
+                    },
                 },
-
             },
         }),
         mounted () {
@@ -132,10 +162,32 @@
                     return false
                 }
             },
+            pickFile () {
+                this.$refs.getImage.click ()
+            },
+            onFilePicked (e) {
+                const files = e.target.files
+                if(files[0] !== undefined) {
+                    this.itens.estadual.file.imageName = files[0].name
+                    if(this.itens.estadual.file.imageName.lastIndexOf('.') <= 0) {
+                        return
+                    }
+                    const fr = new FileReader ()
+                    fr.readAsDataURL(files[0])
+                    fr.addEventListener('load', () => {
+                        this.itens.estadual.file.imageUrl = fr.result
+                        this.itens.estadual.file.imageFile = files[0] // this is an image file that can be sent to server...
+                    })
+                } else {
+                    this.itens.estadual.file.imageName = ''
+                    this.itens.estadual.file.imageFile = ''
+                    this.itens.estadual.file.imageUrl = ''
+                }
+            },
             checkMoney(){
                 if (this.itens.estadual.margem.includes("R$")){
                     console.log('Entrei aqui')
-                    if (this.itens.estadual.margem === 'R$ 0,00' && this.itens.estadual.margemRadio == parseInt(1)){
+                    if (this.itens.estadual.margem === 'R$ 0,00' && this.itens.estadual.margemRadio === "1"){
                         this.showError = true
                         return false
                     }else{
