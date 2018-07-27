eff6c68e006869167dd9ef39688b4493be1dd70e
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-07-27 11:11:57 -0300

    Getters | Implementado verificação de acordo com o tipo da ocupação.

--- src/store/modulesApp/getters.js
+++ src/store/modulesApp/getters.js
@@ -9,3 +9,56 @@ export const getUserType = (state) => {
 export const getStepperPessoal = (state) => {
     return state.stepperPessoal
 }
+
+export const hasFirstCategory = (state) => {
+    // Aposentado - Pensionista - Forças Armadas - Funcionário Publico Federal
+    if (state.stepperPessoal.ocupacao.opcao === '1' || state.stepperPessoal.ocupacao.opcao === '2' ||
+        state.stepperPessoal.ocupacao.opcao === '3' || state.stepperPessoal.ocupacao.opcao === '4' ){
+        return true
+    }else{
+        return false
+    }
+}
+
+export const hasSecondCategory = (state) => {
+    // Funcionário Publico Estadual
+    if (state.stepperPessoal.ocupacao.opcao === '5'){
+        return true
+    }else{
+        return false
+    }
+}
+
+export const hasThirdCategory = (state) => {
+    // Funcionário Publico Municipal
+    if (state.stepperPessoal.ocupacao.opcao === '6'){
+        if (state.stepperPessoal.ocupacao.cidade === 'Recife' || state.stepperPessoal.ocupacao.cidade === 'Jaboatão'){
+            return true
+        }else{
+            return false
+        }
+    }else{
+        return false
+    }
+}
+
+export const hasCityMunicipal = (state) => {
+    // Funcionário Publico Municipal
+    if (state.stepperPessoal.ocupacao.cidade === 'Recife'){
+        return 1
+    }else if (state.stepperPessoal.ocupacao.cidade === 'Jaboatão'){
+        return 2
+    }else{
+        return false
+    }
+}
+
+export const hasFourthCategory = (state) => {
+    // Funcionário Empresa Privada - Autônomo/Liberal - Não trabalha/ Desempregado
+    if (state.stepperPessoal.ocupacao.opcao === '7' || state.stepperPessoal.ocupacao.opcao === '8' ||
+        state.stepperPessoal.ocupacao.opcao === '9' ){
+        return true
+    }else{
+        return false
+    }
+}
