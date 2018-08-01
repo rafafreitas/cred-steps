e64946914621068e64cfb495d03fc724a2829b7f
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-08-01 17:18:09 -0300

    Home | Criação de página inicial.

---
+++ src/views/publics/layouts/Home.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container-contato">
+        <v-layout class="card-content-404" row xs12 wrap>
+            <v-flex xs12 sm12>
+                <div class="logo">
+                    <img class="logo-404" src="../../../assets/clinical.png">
+                </div>
+            </v-flex>
+            <v-flex xs12 sm12>
+                <div class="text">
+                    <h1 class="text-faded">Aprovação rápida de crédito!</h1>
+                </div>
+            </v-flex>
+            <v-flex xs12 sm12>
+                <div class="btn">
+                    <v-btn color="primary" @click="$router.push('/formulario')">Fazer solicitação!</v-btn>
+                </div>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped lang="scss">
+    .logo-404{
+        max-width: 650px;
+    }
+    p{
+        font-size: 14pt;
+    }
+    .card-content-404{
+        margin-top: 30px;
+        .logo{
+            max-width: 650px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .text{
+            max-width: 650px;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .btn{
+            margin-top: 20px;
+            max-width: 187px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    }
+
+</style>
