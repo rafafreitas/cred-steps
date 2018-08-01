6895af90c51b1762ac21b6761302dbd54cf2a530
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-08-01 08:46:00 -0300

    Page Finish | Criado tela final para o fluxo.

--- src/views/pages/layouts/Finish.vue
+++ src/views/pages/layouts/Finish.vue
@@ -8,12 +8,16 @@
             </v-flex>
             <v-flex xs12 sm12>
                 <div class="text">
-                    <p class="text-faded">Desculpe, nada encontrado...<strong>(404)</strong></p>
+                    <p class="text-faded">
+                        Sua solicitação foi enviada!<br>
+                        <strong>Agradecemos seu interesse!</strong>
+
+                    </p>
                 </div>
             </v-flex>
             <v-flex xs12 sm12>
                 <div class="btn">
-                    <v-btn color="info" @click="$router.push('/')">Voltar</v-btn>
+                    <v-btn color="info" @click="$router.push('/')">Fazer uma nova solicitação!</v-btn>
                 </div>
             </v-flex>
         </v-layout>
@@ -22,34 +26,34 @@
 
 <script>
     export default {
-        name: "Page404"
+        name: "Finish"
     }
 </script>
 
 <style scoped lang="scss">
     .logo-404{
-        max-width: 250px;
+        max-width: 450px;
     }
     p{
-     font-size: 14pt;
+        font-size: 14pt;
     }
     .card-content-404{
         margin-top: 30px;
         .logo{
-            max-width: 250px;
+            max-width: 450px;
             margin-left: auto;
             margin-right: auto;
         }
         .text{
-            max-width: 290px;
+            max-width: 240px;
             margin-left: auto;
             margin-right: auto;
+            text-align: center;
         }
         .btn{
-            max-width: 103px;
+            max-width: 259px;
             margin-left: auto;
             margin-right: auto;
         }
     }
-
 </style>
--- src/views/pages/routes/index.js
+++ src/views/pages/routes/index.js
@@ -1,10 +1,20 @@
 import Contato from '../layouts/Contato';
+import Finish from '../layouts/Finish';
 
 export default [
     {
         name: 'Contato',
         path: '/contato',
         component: Contato,
+        meta: {
+            auth: false,
+            userType: null
+        }
+    },
+    {
+        name: 'Finish',
+        path: '/Finish',
+        component: Finish,
         meta: {
             auth: false,
             userType: null
