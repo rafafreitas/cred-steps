34296230386eef23ed928646336d956f348ccbc2
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-08-02 01:09:28 -0300

    Store | Action e classe de serviço  para realizar chamada na api.

---
+++ src/services/Enderecos.js
@@ -0,0 +1,22 @@
+import httpClient from './../helpers/axios';
+
+class EnderecosService {
+
+    constructor() {
+    }
+
+    getStates() {
+        return new Promise((resolve, reject) => {
+            httpClient.get('/estados')
+                .then(response => {
+                    resolve(response)
+                })
+                .catch(error => {
+                    reject(error)
+                })
+                .finally(() => console.log('Fim'))
+        })
+    }
+}
+
+export default new EnderecosService();
--- src/store/modulesApp/actions.js
+++ src/store/modulesApp/actions.js
@@ -1,3 +1,22 @@
-/*
-    export actions
- */
+import Enderecos from "./../../services/Enderecos"
+
+export const getStates = (context) => {
+    return new Promise((resolve, reject) => {
+        Enderecos.getStates()
+            .then(
+                (result) => {
+                    context.commit('setStates', result.data.result)
+                    console.log('Estados Carregados')
+                    resolve(result)
+                },
+                (error) => {
+                    console.log('Action_Erro', error)
+                    reject(error)
+                }
+            ).catch(
+            (err) =>{
+                console.log('catch Action_Erro', error)
+                reject(err)
+            })
+    })
+}
